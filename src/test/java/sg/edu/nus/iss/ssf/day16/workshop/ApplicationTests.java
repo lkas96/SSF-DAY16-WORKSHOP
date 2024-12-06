282f065f18f7f633fe974704e6098907a2fed5f1
lkas96 <kwek_lawson@hotmail.com>
2024-12-06 13:34:16 +0800

    Springboot template

---
+++ src/test/java/sg/edu/nus/iss/ssf/day16/workshop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf.day16.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
